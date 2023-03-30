06d54796dbe6f15770cb9b64534fe2a40e2784f8
renaudghx <renaudgheux@gmail.com>
2023-03-30 18:01:58 +0200

    Ajout Flower spin + Fade-in section + Title scroll

--- wp-content/themes/foce-child/functions.php
+++ wp-content/themes/foce-child/functions.php
@@ -1,9 +1,9 @@
 <?php
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles', 20);
 function theme_enqueue_styles() {
+    wp_enqueue_script( 'theme-scripts', get_stylesheet_directory_uri() . '/js/scripts.js', array(), '1.0', true );
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
-    wp_enqueue_script('theme-script',get_template_directory_uri() . '/js/script.js' , array(), false, false);
 }
 
 // Get customizer options form parent theme
---
+++ wp-content/themes/foce-child/js/scripts.js
@@ -0,0 +1,28 @@
+// Remove the transition class
+const titleStory = document.querySelector('.title-story');
+titleStory.classList.remove('title-story');
+const titleStudio = document.querySelector('.title-studio');
+titleStudio.classList.remove('title-studio');
+
+function titleObserver(classAnim, classObserve, nameConst){
+
+  // Create the observer :
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      // If the element is visible
+      if (entry.isIntersecting) {
+        // Add the animation class
+        nameConst.classList.add(classAnim);
+        return;
+      }
+      // We're not intersecting, so remove the class!
+      nameConst.classList.remove(classAnim);
+    });
+  });
+  
+  // Tell the observer which elements to track
+  observer.observe(document.querySelector(classObserve));
+}
+
+titleObserver('title-story','.observe-story', titleStory);
+titleObserver('title-studio','.observe-studio', titleStudio);
